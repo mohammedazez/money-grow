b0b920bb36da26245b82bd74b47e509a036bd4c5
ATLAP0103\HP <muhamadaziz047@gmail.com>
2024-09-10 22:33:08 +0700

    add middleware

--- public/index.php
+++ public/index.php
@@ -2,13 +2,24 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Middleware\BodyParsingMiddleware;
+
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+// This is for routing middleware
+$app->addRoutingMiddleware();
+
+// This is for parsing JSON & form data
+$app->addBodyParsingMiddleware();
+
+// This is for handling middleware optional but useful for debugging
+$app->addErrorMiddleware(true, true, true);
+
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Welcome to money grow!");
     return $response;
 });
 
