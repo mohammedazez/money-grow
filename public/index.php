0e1cd854f81c79f920b50d7ac4ed58867c6d83a1
ATLAP0103\HP <muhamadaziz047@gmail.com>
2024-09-10 22:48:00 +0700

    make try catch for loan validator in controller

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use App\Controllers\LoanController;
 use Slim\Middleware\BodyParsingMiddleware;
 
 
--- src/Controllers/LoanController.php
+++ src/Controllers/LoanController.php
@@ -1,12 +1,26 @@
 <?php
 
+namespace App\Controllers;
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Validators\LoanValidator;
+
 
 class LoanController
 {
     public function apply(Request $request, Response $response)
     {
+        $data = $request->getParsedBody();
+
+        $validator = new LoanValidator();
+        try {
+            $validator->validate($data);
+        } catch (\Respect\Validation\Exceptions\NestedValidationException $exception) {
+            $errors = $exception->getMessages();
+            return $response->withStatus(400)->withHeader('Content-Type', 'application/json')
+                ->getBody()->write(json_encode(['errors' => $errors]));
+        }
 
     }
 }
