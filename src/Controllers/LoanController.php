fe5436066a53b6cdb0012b58acadf97d50c19a7b
ATLAP0103\HP <muhamadaziz047@gmail.com>
2024-09-11 12:41:40 +0700

    more readable logic

--- src/Controllers/LoanController.php
+++ src/Controllers/LoanController.php
@@ -12,28 +12,34 @@ class LoanController
     public function apply(Request $request, Response $response)
     {
         $data = $request->getParsedBody();
-
         $validator = new LoanValidator();
+
         try {
             $validator->validate($data);
+
+            // logic to save load file
+            $logFilePath = __DIR__ . '/../../logs/loan_applications.txt';
+
+            if (!is_dir(dirname($logFilePath))) {
+                mkdir(dirname($logFilePath), 0777, true);
+            }
+    
+            $logData = json_encode($data) . PHP_EOL;
+    
+            file_put_contents($logFilePath, $logData, FILE_APPEND);
+
+            $response->getBody()->write(json_encode([
+                'message' => 'Application submitted successfully.'
+            ]));
+            return $response->withHeader('Content-Type', 'application/json')
+                            ->withStatus(200);
+                            
         } catch (\Respect\Validation\Exceptions\NestedValidationException $exception) {
-            $errors = $exception->getMessages();
-            return $response->withStatus(400)->withHeader('Content-Type', 'application/json')
-                ->getBody()->write(json_encode(['errors' => $errors]));
+            $response->getBody()->write(json_encode([
+                'errors' => $exception->getMessages()
+            ]));
+            return $response->withHeader('Content-Type', 'application/json')
+                            ->withStatus(400);
         }
-        
-          $logFilePath = __DIR__ . '/../../logs/loan_applications.txt';
-
-          if (!is_dir(dirname($logFilePath))) {
-              mkdir(dirname($logFilePath), 0777, true);
-          }
-  
-          $logData = json_encode($data) . PHP_EOL;
-  
-          file_put_contents($logFilePath, $logData, FILE_APPEND);
-  
-          $response->getBody()->write(json_encode(['message' => 'Application submitted successfully.']));
-          return $response->withHeader('Content-Type', 'application/json');
-
     }
 }
