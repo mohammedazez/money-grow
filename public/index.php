dc809e15c012c66a9d6c1de879bbfb70b1b25b1a
ATLAP0103\HP <muhamadaziz047@gmail.com>
2024-09-11 12:41:52 +0700

    update

--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use App\Controllers\LoanController;
-use Slim\Middleware\BodyParsingMiddleware;
-
 
 require __DIR__ . '/../vendor/autoload.php';
 
